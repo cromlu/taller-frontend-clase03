42499d636d9772c4ce88b8ec76336ba963139cf5
Joel Gonzales Tipismana <joelengt@gmail.com>
2016-01-08 18:52:48 -0500

    clase 03 frontend

---
+++ ejemplos/js/code.js
@@ -0,0 +1,11 @@
+var btn = $('#boton');
+
+btn.on("click", Ha);
+
+// var caja = $('#caja');
+var caja = document.getElementById('caja');
+
+function Ha (){
+	alert(caja.value);
+	// alert("saludos");
+}
